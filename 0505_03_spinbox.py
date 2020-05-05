75bd1a248a3160babae22c9d247861dd6c930761
yzh <351268691@qq.com>
2020-05-05 23:35:35 +0800

    sliderbutton

---
+++ 0505_03_spinbox.py
@@ -0,0 +1,37 @@
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import Qt
+
+
+# 单选按钮
+class Spinbox(QWidget):
+    def __init__(self):
+        super(Spinbox, self).__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle('QSpinBox')
+        self.resize(200, 100)
+        self.label = QLabel('当前数值：')
+        self.sb = QSpinBox()
+        self.sb.setValue(66)
+        self.sb.setRange(66, 88)
+        # self.sb.setSingleStep(3)
+        self.sb.valueChanged.connect(self.valueChange)
+
+        vlayout = QVBoxLayout()
+        vlayout.addWidget(self.label)
+        vlayout.addWidget(self.sb)
+        self.setLayout(vlayout)
+
+    def valueChange(self):
+        self.label.setText('当前数值：' + str(self.sb.value()))
+        print(self.sb.value())
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    spinwin = Spinbox()
+    spinwin.show()
+    sys.exit(app.exec_())
---
+++ 0505_04_slider.py
@@ -0,0 +1,40 @@
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import Qt
+
+
+# 单选按钮
+class Slider(QWidget):
+    def __init__(self):
+        super(Slider, self).__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle('QSlider')
+        self.resize(200, 100)
+        self.label = QLabel('声音大小：50')
+        self.sd = QSlider(Qt.Horizontal)
+        self.sd.setValue(50)
+        # 默认0到99
+        # self.sd.setMinimum(3)
+        # self.sd.setMaximum(1000)
+        # self.sd.setRange(3, 1000)
+        self.sd.setSingleStep(10)
+        self.sd.setTickPosition(QSlider.TicksBelow)
+        self.sd.valueChanged.connect(self.valueChange)
+
+        vlayout = QVBoxLayout()
+        vlayout.addWidget(self.label)
+        vlayout.addWidget(self.sd)
+        self.setLayout(vlayout)
+
+    def valueChange(self):
+        self.label.setText('声音大小：' + str(self.sd.value()))
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    mainwin = Slider()
+    mainwin.show()
+    sys.exit(app.exec_())

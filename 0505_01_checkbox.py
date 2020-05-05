7fcf973ca85bc870506dd4aeab2e739ceb439e74
yzh <351268691@qq.com>
2020-05-05 22:16:39 +0800

    radiobutton

---
+++ 0505_01_checkbox.py
@@ -0,0 +1,47 @@
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import Qt
+
+
+# 单选按钮
+class Checkbox(QWidget):
+    def __init__(self):
+        super(Checkbox, self).__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle('QCheckBox')
+        hlayout = QHBoxLayout()
+
+        self.button1 = QCheckBox('未选中')
+        # 也可这样设置两种状态
+        # self.button1.setCheckState(Qt.Checked)
+        self.button2 = QCheckBox('选中')
+        self.button2.setChecked(True)
+        self.button3 = QCheckBox('半选中')
+        # 允许半选中状态，设置三态
+        self.button3.setTristate(True)
+        # 设置默认半选中
+        self.button3.setCheckState(Qt.PartiallyChecked)
+
+        self.button3.stateChanged.connect(lambda: self.buttonstate(self.button3))
+        self.button1.stateChanged.connect(lambda: self.buttonstate(self.button1))
+        self.button2.stateChanged.connect(lambda: self.buttonstate(self.button2))
+
+        hlayout.addWidget(self.button1)
+        hlayout.addWidget(self.button2)
+        hlayout.addWidget(self.button3)
+
+        self.setLayout(hlayout)
+
+    def buttonstate(self, btn):
+        print(btn.text() + '：' + str(btn.checkState()))
+        print(self.button3.text() + '：' + str(self.button3.checkState()))
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    checkwin = Checkbox()
+    checkwin.show()
+    sys.exit(app.exec_())
---
+++ 0505_02_combobox.py
@@ -0,0 +1,47 @@
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import Qt
+
+
+# 单选按钮
+class Combobox(QWidget):
+    def __init__(self):
+        super(Combobox, self).__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle('QComboBox')
+        self.resize(300, 120)
+        vlayout = QVBoxLayout()
+        self.label1 = QLabel('语言选择：')
+        self.cb = QComboBox()
+        self.cb.addItems(['C语言', 'Python', 'Java', 'Go'])
+        self.cb.currentIndexChanged.connect(self.selectionChange)
+        self.label2 = QLabel('感兴趣程度：')
+        self.cb2 = QComboBox()
+        self.cb2.addItems(['喜欢', '超爱', '无感', '厌恶'])
+
+        vlayout.addWidget(self.label1)
+        vlayout.addWidget(self.cb)
+        vlayout.addWidget(self.label2)
+        vlayout.addWidget(self.cb2)
+
+        self.setLayout(vlayout)
+
+    # 默认传入当前索引
+    def selectionChange(self, i):
+        newlabel1 = '语言选择：' + self.cb.currentText()
+        self.label1.setText(newlabel1)
+        # 根据内容自适应大小
+        self.label1.adjustSize()
+        for count in range(self.cb.count()):
+            print('item' + str(count) + '=' + self.cb.itemText(count))
+        print('当前索引：', i, '，编程：', self.cb.currentText())
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    combowin = Combobox()
+    combowin.show()
+    sys.exit(app.exec_())

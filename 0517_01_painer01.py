f83ac71c8ba671e177066ee2a8451d0f0f53ca09
yzh <351268691@qq.com>
2020-05-18 00:11:59 +0800

    0517_01_painter01

---
+++ 0517_01_painer01.py
@@ -0,0 +1,32 @@
+import sys
+import math
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+
+
+class PainterPi(QWidget):
+    def __init__(self):
+        super(PainterPi, self).__init__()
+        self.resize(300, 300)
+        self.setWindowTitle('绘图')
+
+    def paintEvent(self, QPaintEvent):
+        painter = QPainter()
+        painter.begin(self)
+        # painter.setPen(Qt.red)
+        size = self.size()
+
+        for i in range(1000):
+            x = 100.0 * math.sin(-1 * math.pi + math.pi * 2.0 * (i/1000)) + 150
+            y = 50.0 * math.cos(-1 * math.pi + math.pi * 2.0 * (i/1000)) + 150
+            painter.drawPoint(x, y)
+
+        painter.end()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    mainwin = PainterPi()
+    mainwin.show()
+    sys.exit(app.exec_())
